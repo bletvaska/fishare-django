f0e41d528c6cf86d6eb488e4c9fdb43ac4b34770
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-23 15:20:44 +0200

    files model refactoring

---
+++ fishare/files/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+# python manage.py makemigrations
+# python manage.py migrate
+class File(models.Model):
+    filename = models.CharField(max_length=128)
+    downloads = models.IntegerField(default=0)
+    max_downloads = models.IntegerField(default=1)
+    size = models.IntegerField()
+    mime_type = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
