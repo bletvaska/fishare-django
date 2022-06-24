8205992c1c3435c5553a8a2c98d8079d9ba4c72b
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-24 11:35:29 +0200

    refaktoring of model File

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the fishare_project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-f+c=v9nrrbb+3@6w4cq(rhq=f#b*t2syy6w+r!$xhx24!7ru0#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -81,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -111,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -122,7 +117,6 @@ TIME_ZONE = 'Europe/Bratislava'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -137,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'storage'
--- fishare/files/models.py
+++ fishare/files/models.py
@@ -1,18 +1,46 @@
+import string
+from random import choices
+
+from django.conf import settings
 from django.db import models
+
+
+def slugify(length=7):
+    return ''.join(choices(string.ascii_letters + string.digits, k=length))
 
 
 # Create your models here.
 # python manage.py makemigrations
 # python manage.py migrate
 class File(models.Model):
-    filename = models.CharField(max_length=128)
-    downloads = models.IntegerField(default=0)
+    slug = models.CharField(default=slugify, max_length=7, unique=True, null=None)
+    filename = models.CharField(max_length=128, editable=False)
+    downloads = models.IntegerField(default=0, editable=False)
     max_downloads = models.IntegerField(default=1)
-    size = models.IntegerField('File Size')
-    mime_type = models.CharField(max_length=64)
+    size = models.IntegerField('File Size', editable=False)
+    mime_type = models.CharField(max_length=64, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    file = models.FileField(null=False)
+
+    def save(self, *args, **kwargs):
+        print(args)
+        print(kwargs)
+        # if self.id is None:
+        self.filename = self.file.name
+        self.size = self.file.size
+        # if type(self.file.file) != BufferedReader:
+        self.mime_type = self.file.file.content_type
+
+        super().save(args, kwargs)
+
+    def delete(self, *args, **kwargs):
+        # delete from filesystem first
+        path = settings.MEDIA_ROOT / self.filename
+        path.unlink(missing_ok=True)
+
+        # delete from db
+        super().delete(args, kwargs)
 
     def __str__(self):
         return f'{self.filename}'
-
