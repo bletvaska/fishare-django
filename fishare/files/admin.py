d3fca6bd1d540b7970c82d49c9c46940b31a39bf
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-24 09:40:51 +0200

    refactoring of admin interface

--- fishare/files/admin.py
+++ fishare/files/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from fishare.files.models import File
 
-admin.site.register(File)
+
+# admin.site.register(File)
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'mime_type', 'size', 'get_downloads', 'created_at']
+    ordering = ['-created_at']
+    list_filter = ['mime_type']
+    search_fields = ['filename']
+
+    def get_downloads(self, file: File):
+        return f'{file.downloads}/{file.max_downloads}'
+
+    get_downloads.short_description = 'Downloads'
--- fishare/files/models.py
+++ fishare/files/models.py
@@ -8,7 +8,11 @@ class File(models.Model):
     filename = models.CharField(max_length=128)
     downloads = models.IntegerField(default=0)
     max_downloads = models.IntegerField(default=1)
-    size = models.IntegerField()
+    size = models.IntegerField('File Size')
     mime_type = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.filename}'
+
