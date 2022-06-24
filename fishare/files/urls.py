a068bc2bd404caece62179b6bb99c59dd824374e
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-24 13:38:14 +0200

    added urls.py in files app

---
+++ fishare/files/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from fishare.files import views
+
+app_name = 'files'
+
+urlpatterns = [
+    path(
+        '<str:slug>',
+        views.download_file,
+        name='download_file'
+    )
+]
