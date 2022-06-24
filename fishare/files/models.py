02a2719f96064b307bdd54f7d6460b34dcb640d8
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-24 13:09:21 +0200

    saving the file with the slug name

--- fishare/files/models.py
+++ fishare/files/models.py
@@ -13,7 +13,7 @@ def slugify(length=7):
 # python manage.py makemigrations
 # python manage.py migrate
 class File(models.Model):
-    slug = models.CharField(default=slugify, max_length=7, unique=True, null=None)
+    slug = models.CharField(default=slugify, max_length=7, unique=True, null=None, editable=False)
     filename = models.CharField(max_length=128, editable=False)
     downloads = models.IntegerField(default=0, editable=False)
     max_downloads = models.IntegerField(default=1)
@@ -21,16 +21,20 @@ class File(models.Model):
     mime_type = models.CharField(max_length=64, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    file = models.FileField(null=False)
+    file = models.FileField(null=False, )
 
     def save(self, *args, **kwargs):
         print(args)
         print(kwargs)
         # if self.id is None:
+
+        # set the metadata based on uploaded file
         self.filename = self.file.name
-        self.size = self.file.size
-        # if type(self.file.file) != BufferedReader:
         self.mime_type = self.file.file.content_type
+        self.size = self.file.size
+
+        # renaming the file on filesystem
+        self.file.name = self.slug
 
         super().save(args, kwargs)
 
