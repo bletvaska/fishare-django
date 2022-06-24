9c71bcfdd8dfcfec65d214180d5a297271313812
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-24 09:43:03 +0200

    refactoring of admin interface

--- fishare/files/admin.py
+++ fishare/files/admin.py
@@ -8,7 +8,7 @@ from fishare.files.models import File
 
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
-    list_display = ['filename', 'mime_type', 'size', 'get_downloads', 'created_at']
+    list_display = ['filename', 'mime_type', 'format_filesize', 'get_downloads', 'created_at']
     ordering = ['-created_at']
     list_filter = ['mime_type']
     search_fields = ['filename']
@@ -17,3 +17,15 @@ class FileAdmin(admin.ModelAdmin):
         return f'{file.downloads}/{file.max_downloads}'
 
     get_downloads.short_description = 'Downloads'
+
+    def format_filesize(self, file: File, suffix="B"):
+        size = 0
+        if file.size:
+            size = file.size
+        for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+            if abs(size) < 1024.0:
+                return f"{size:3.1f}{unit}{suffix}"
+            size /= 1024.0
+        return f"{size:.1f}Yi{suffix}"
+
+    format_filesize.short_description = 'Size'
