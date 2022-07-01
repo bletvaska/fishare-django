3b7bee857ef9e48e2358b926fc28e2aa731c7954
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-07-01 09:17:47 +0200

    forgotten urls.py

---
+++ fishare/error_handlers/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from fishare.error_handlers.views import not_found_view
+
+urlpatterns = [
+    path('404/', not_found_view)
+]
+
+#handler404 = 'fishare.error_handlers.views.not_found_view'
