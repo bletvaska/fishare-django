9aa5cf891e89ab6bef92817f5ca5e490abff2673
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-07-12 13:57:39 +0200

    another tests

--- fishare/files/tests/test_models.py
+++ fishare/files/tests/test_models.py
@@ -9,29 +9,35 @@ def fake():
     yield Faker()
 
 
-@pytest.mark.django_db
-def test_when_email_is_entered_then_it_should_be_in_email_field(fake):
-    # arrange
+@pytest.fixture()
+def user(fake):
     email = fake.email()
     username = fake.user_name()
     password = fake.password()
 
+    yield username, email, password
+
+
+@pytest.mark.django_db
+def test_when_email_is_entered_then_it_should_be_in_email_field(user):
     # act
-    user = User.objects.create(username=username, email=email, password=password)
+    u = User.objects.create(username=user[0], email=user[1], password=user[2])
 
     # assert
-    assert user.email == email, "The email used during creation is not the same."
+    assert u.email == user[1], "The email used during creation is not the same."
 
 
 @pytest.mark.django_db
-def test_when_two_users_with_same_username_are_created_then_raise_exception(fake):
-    # arrange
-    email = fake.email()
-    username = fake.user_name()
-    password = fake.password()
-
+def test_when_two_users_with_same_username_are_created_then_raise_exception(user):
     # act
-    User.objects.create(username=username, email=email, password=password)
+    User.objects.create(username=user[0], email=user[1], password=user[2])
 
+    # assert
     with pytest.raises(IntegrityError):
-        User.objects.create(username=username, email=email, password=password)
+        User.objects.create(username=user[0], email=user[1], password=user[2])
+
+
+@pytest.mark.django_db
+def test_when_username_is_none_then_raise_integrityerror_exception(user):
+    with pytest.raises(IntegrityError):
+        User.objects.create(username=None)
