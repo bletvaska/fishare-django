72ad28b5da24d160b7d6e4c48761f31616692a0a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-07-12 14:56:03 +0200

    additional rest api tests + refactoring

--- fishare/files/tests/test_api.py
+++ fishare/files/tests/test_api.py
@@ -8,16 +8,24 @@ def api_client():
     yield APIClient()
 
 
-@pytest.mark.django_db
-def test_when_files_list_is_retrieved_then_status_is_200(api_client):
+@pytest.fixture()
+def response(api_client):
     url = reverse('files:api-collection')
     response = api_client.get(url)
+    yield response
+
+
+@pytest.mark.django_db
+def test_when_files_list_is_retrieved_then_status_is_200(response):
     assert response.status_code == 200, "Should be 200 on HTTP GET."
 
 
 @pytest.mark.django_db
-def test_when_response_is_returned_then_type_of_response_is_json(api_client):
-    url = reverse('files:api-collection')
-    response = api_client.get(url)
+def test_when_response_is_returned_then_type_of_response_is_json(response):
     assert response.headers.get('content-type') == 'application/json', "Response type should be JSON."
 
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("key", ['count', 'next', 'previous', 'results'])
+def test_when_response_is_returned_then_specific_keys_should_be_specific(key, response):
+    assert key in response.json()
